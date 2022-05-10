2943ea8c6cbde80f590153e5204cf99bbe75a546
sainsaji <95070425+sainsaji@users.noreply.github.com>
2022-05-10 12:52:25 +0530

    admin integration
    changed db name to myapp

--- admin/Client/index.js
+++
@@ -1,9 +0,0 @@
-var myapp=angular.module("myapp",[]);
-
-myapp.controller("myctrl",function($scope,$http){
-    $http.get("http://127.0.0.1:5500/display").success(function(response){$scope.names=response;});
-    console.log("json loading success");
-});
-
-
-
